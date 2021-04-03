28c9c07f9180ebc014a39960af16355cab9037c5
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-04-03 11:50:14 +0200

    Completata la one-to-one

---
+++ database/seeds/AuthorSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Author;
+use App\AuthorDetail;
+
+class AuthorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+
+        for ($i = 0; $i< 25; $i++){
+
+            $author = new Author();
+            $author->name = $faker->firstname();
+            $author->surname = $faker->lastname();
+            $author->email = $faker->email();
+            $author->save();
+
+            $authorDetail = new AuthorDetail();
+            $authorDetail->bio = $faker->text();
+            $authorDetail->website = $faker->url();
+            $authorDetail->pic = 'https://picsum.photos/seed/'. rand(0, 1000) . '/200/300';
+
+            $author->detail()->save($authorDetail);
+
+        }
+
+    }
+}
